eab85dbf550a3477636006eccd41a3139f551334
mapan <mapan0817@gmail.com>
2021-03-22 18:24:27 -0700

    Update collections view with all or slug.

--- leafstore/store/views.py
+++ leafstore/store/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 
 from .models import Collection, Product, ProductColor, Picture
@@ -12,10 +12,19 @@ def story(request):
   return render(request, 'store/story.html')
 
 
-def collection(request, slug):
-  collection = Collection.objects.get(slug=slug)
-  products = collection.products.all()
-  context = {'products': products}
+def collection(request, slug=None):
+  collection = None
+  collections = Collection.objects.all()
+  products = Product.objects.filter(colors__stock__gt=0)
+  if slug:
+    collection = get_object_or_404(Collection, slug=slug)
+    collections = [collection]
+    products = products.filter(collection=collection)
+
+  context = {'collection': collection,
+             'collections': collections,
+             'products': products}
+  
   return render(request, 'store/collection.html', context)
 
 

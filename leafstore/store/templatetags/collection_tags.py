fabbf86931eb4af821d01e9dc959b2e3f01bf88e
mapan <mapan0817@gmail.com>
2021-03-25 21:28:26 -0700

    Add product images carousel view.

--- leafstore/store/templatetags/collection_tags.py
+++ leafstore/store/templatetags/collection_tags.py
@@ -1,5 +1,5 @@
 from django import template
-from ..models import Collection, ProductColor
+from ..models import Collection, ProductColor, Picture
 
 register = template.Library()
 
@@ -13,3 +13,9 @@ def get_all_collections():
 def get_product_colors(product):
   return ProductColor.objects.filter(
       product__id=product.id).filter(stock__gt=0)
+
+
+@register.simple_tag
+def get_product_pictures(product):
+  colors = get_product_colors(product)
+  return Picture.objects.filter(color__in=colors)
